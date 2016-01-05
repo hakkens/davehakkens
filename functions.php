0662ab1e5945cd42f372c25a8731512b8b792014
Dave Hakkens <mail@davehakkens.nl>
2016-01-05 21:40:52 +0100

    Whoops
    put back the crucial line

--- functions.php
+++ functions.php
@@ -154,3 +154,5 @@ function dave_hakkens_scripts() {
   wp_enqueue_script( 'dh_plugins', get_bloginfo( 'template_url' ) . '/js/plugins.js', array( 'jquery' ) );
   wp_enqueue_script( 'dh_main', get_bloginfo( 'template_url' ) . '/js/main.js', array( 'jquery', 'fancybox', 'isotope', 'scroll_to', 'mousewheel', 'dh_plugins' ) );
 }
+
+add_action( 'wp_enqueue_scripts', 'dave_hakkens_scripts' );
