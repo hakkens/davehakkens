61d927584ac8d6549752777cd2378dc20a2f891f
Dave Hakkens <mail@davehakkens.nl>
2016-02-09 22:53:02 +0100

    Added spam notification after register
    Got some complains about mail in spamfolder.

--- buddypress/members/register.php
+++ buddypress/members/register.php
@@ -352,7 +352,7 @@
       do_action( 'bp_before_registration_confirmed' ); ?>
 
       <?php if ( bp_registration_needs_activation() ) : ?>
-        <p><?php _e( 'You have successfully created your account! To begin using this site you will need to activate your account via the email we have just sent to your address.', 'buddypress' ); ?></p>
+        <p><?php _e( 'You have successfully created your account! To begin using this site you will need to activate your account via the email we have just sent to your address. (make sure to check your spam-folder)', 'buddypress' ); ?></p>
       <?php else : ?>
         <p><?php _e( 'You have successfully created your account! Please log in using the username and password you have just created.', 'buddypress' ); ?></p>
       <?php endif; ?>
