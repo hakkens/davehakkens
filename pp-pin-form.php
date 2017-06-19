1d8cf34ef9d7fd736948c54f4ad2d0ab778209ca
Ben Naylor <ben.naylor@beethree.nl>
2017-06-19 20:24:37 +0200

    pins input page skeleton

---
+++ pp-pin-form.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  Template Name: PP Pin Form
+  Description: Precious Plastic Pin input form for users
+*/
+
+get_header();
+get_template_part( 'navbar' );
+
+?>
+
+<div id="content">
+  Pin form goes here
+</div>
+
+<?php get_footer(); ?>
