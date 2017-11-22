f1b343958d45542376025361fd477e90da63f521
Dave Hakkens <dave@davehakkens.nl>
2017-11-22 20:38:37 +0100

    Rights for contributers
    Add rights for moderaters to delete users + bulk edit spam

--- functions.php
+++ functions.php
@@ -351,6 +351,102 @@ if ( is_active_sidebar( 'primary' ) ) {
 }
 
 
+
+/*
+ * Let contributor manage users, and run this only once.
+ */
+function isa_contributor_manage_users() {
+
+    if ( get_option( 'isa_add_cap_contributor_once' ) != 'done' ) {
+
+        // let contributor manage users
+
+        $edit_contributor = get_role('contributor'); // Get the user role
+        $edit_contributor->add_cap('edit_users');
+        $edit_contributor->add_cap('list_users');
+        $edit_contributor->add_cap('promote_users');
+        $edit_contributor->add_cap('create_users');
+        $edit_contributor->add_cap('add_users');
+        $edit_contributor->add_cap('delete_users');
+
+        update_option( 'isa_add_cap_contributor_once', 'done' );
+    }
+
+}
+add_action( 'init', 'isa_contributor_manage_users' );
+
+//prevent contributor from deleting, editing, or creating an administrator
+// only needed if the contributor was given right to edit users
+
+class ISA_User_Caps {
+
+  // Add our filters
+  function __construct() {
+    add_filter( 'editable_roles', array(&$this, 'editable_roles'));
+    add_filter( 'map_meta_cap', array(&$this, 'map_meta_cap'),10,4);
+  }
+  // Remove 'Administrator' from the list of roles if the current user is not an admin
+  function editable_roles( $roles ){
+    if( isset( $roles['administrator'] ) && !current_user_can('administrator') ){
+      unset( $roles['administrator']);
+    }
+    return $roles;
+  }
+  // If someone is trying to edit or delete an
+  // admin and that user isn't an admin, don't allow it
+  function map_meta_cap( $caps, $cap, $user_id, $args ){
+    switch( $cap ){
+        case 'edit_user':
+        case 'remove_user':
+        case 'promote_user':
+            if( isset($args[0]) && $args[0] == $user_id )
+                break;
+            elseif( !isset($args[0]) )
+                $caps[] = 'do_not_allow';
+            $other = new WP_User( absint($args[0]) );
+            if( $other->has_cap( 'administrator' ) ){
+                if(!current_user_can('administrator')){
+                    $caps[] = 'do_not_allow';
+                }
+            }
+            break;
+        case 'delete_user':
+        case 'delete_users':
+            if( !isset($args[0]) )
+                break;
+            $other = new WP_User( absint($args[0]) );
+            if( $other->has_cap( 'administrator' ) ){
+                if(!current_user_can('administrator')){
+                    $caps[] = 'do_not_allow';
+                }
+            }
+            break;
+        default:
+            break;
+    }
+    return $caps;
+  }
+
+}
+
+$isa_user_caps = new ISA_User_Caps();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* Modified from  'mycred_display_users_badges' to just display selected badges, TODO: pass argumment and merge upstream */
 if ( ! function_exists( 'mycred_display_custom_users_badges' ) ) {
     function mycred_display_custom_users_badges( $user_id = NULL, $width = MYCRED_BADGE_WIDTH, $height = MYCRED_BADGE_HEIGHT ) {
