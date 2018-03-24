01b2089f0c39190c3ba804d85f908fa9537a9d23
Alejandro Romero <alromh87@gmail.com>
2018-03-24 03:11:28 +0000

    Changed profile page to ue latestImages widget

--- buddypress/members/single/profile/profile-loop.php
+++ buddypress/members/single/profile/profile-loop.php
@@ -167,33 +167,7 @@ do_action( 'bp_before_profile_loop_content' ); ?>
 <div class="user-attachments">
 
 <?php
-$the_query = new WP_Query( array( 'post_type' => 'attachment', 'post_status' => 'inherit', 'posts_per_page' => 6, 'author' => bp_displayed_user_id(), 'post_parent__not_in' => array(0) ) );
-if ( $the_query->have_posts() ) while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-  <?php
-
-  $link = get_permalink($post->post_parent);
-
-  if(strpos($link, '/reply/')){ //link back to topic page instead of reply page
-    $reply_post = get_post($post->post_parent);
-    //calculate pagination
-    $argsX = array(
-      'post_type' => 'reply',
-      'numberposts' => -1,
-      'post_status' => 'publish',
-      'orderby' => 'date',
-      'order' => 'ASC',
-      'post_parent' => $reply_post->post_parent,
-      'fields'      => 'ids',
-    );
-    $siblings = get_posts($argsX);
-    $indx = array_search($post->post_parent, $siblings);
-    $page = floor($indx/get_option('_bbp_replies_per_page'));
-
-    $link = get_permalink($reply_post->post_parent). ( $page > 0 ? "page/" . ( $page + 1 ) . "/" : "" ) . "#post-" . $post->post_parent;
-  }
-  ?>
-  <a href="<?php echo $link; ?>"><div class="image" style="background-image: url(<?php echo wp_get_attachment_thumb_url(get_the_ID()); ?>);"></div></a><?php
-endwhile;
+  the_widget("Latest_Community_Uploads", array("max"=>"6", "user_id"=>bp_displayed_user_id()));
 ?>
 
 </div>
